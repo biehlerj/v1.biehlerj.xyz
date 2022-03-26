7e725213ba49132fdf2a852cee126920197abbdd
Jacob Biehler <biehlerj@protonmail.com>
2022-03-25 20:31:22 -0700

    Adding webpack

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 const isProd = process.env.NODE_ENV === "production";
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: isProd ? "/biehlerj.xyz/" : "",
+const nextConfig = withBundleAnalyzer({
+    assetPrefix: isProd ? "/" : "",
     trailingSlash: true,
-};
+
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && isServer) {
+            const originalEntry = config.entry;
+
+            config.entry = async () => {
+                const entries = await originalEntry();
+
+                return { ...entries };
+            };
+        }
+        return config;
+    }
+});
 
 module.exports = nextConfig;
