6dfd1d4edeabee1cd2828cbfa6b63319f5865397
Jacob Biehler <biehlerj@protonmail.com>
2022-03-25 19:02:39 -0700

    Fixing next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: process.env.NODE_ENV === "production" ? "/name-of-repository" : ""
+    assetPrefix: process.env.NODE_ENV === "production" ? "/biehlerj.xyz" : ""
 };
 
 module.exports = nextConfig;
