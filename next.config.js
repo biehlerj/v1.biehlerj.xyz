3c2105b603494f8913fecd717d5459c0e463345d
Jacob Biehler <biehlerj@protonmail.com>
2022-03-25 21:38:39 -0700

    Removing bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,30 +1,14 @@
 const isProd = process.env.NODE_ENV === "production";
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-});
+
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
+const nextConfig = {
     assetPrefix: isProd ? "/" : "",
     trailingSlash: true,
-
     images: {
         loader: "cloudinary",
         path: "https://res.cloudinary.com/dpnmq46pq/image/upload/"
-    },
-
-    webpack: (config, { dev, isServer }) => {
-        if (!dev && isServer) {
-            const originalEntry = config.entry;
-
-            config.entry = async () => {
-                const entries = await originalEntry();
-
-                return { ...entries };
-            };
-        }
-        return config;
     }
-});
+};
 
 module.exports = nextConfig;
