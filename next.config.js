c331f7d810ca743a9593450fc649bf5473164062
Jacob Biehler <jacob@biehlerj.xyz>
2023-04-09 23:20:49 -0700

    Updating next config to fix image issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProd ? "/" : "",
+    assetPrefix: isProd ? "/biehlerj.github.io/" : "",
     trailingSlash: true,
     images: {
         loader: "cloudinary",
